a96b7c542b5242d8b05bb5649024c59226259407
Eli Hansen <15258789+emh68@users.noreply.github.com>
2024-05-21 16:00:54 -0600

    edits

--- Treasure Island Game/Treasure Island Game.py
+++ Treasure Island Game/Treasure Island Game.py
@@ -21,31 +21,31 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************
 ''')
 print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.")
+print("Your mission is to find the treasure.\n")
 
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+# https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
-plane = input("""Your plane has an engine on fire. \nDo you stay with the plane and try to land or use the parachute and jump out? 
-Type stay or jump
-\n""")
+choice1 = input('Your plane has an engine on fire.\n'
+                'Do you stay with the plane and try to land or use the parachute and jump out? Type stay or jump ').lower()
 
-if plane == "jump":
-    river = input("""\nNice! \nYou jump out and your parachute snags in a tree. \nYou detach from your parachute and come to a river crossing. 
-Do you cross the river or keep walking in the jungle and try go around? 
-Type cross or go around\n\n""")
+if choice1 == 'jump':
+    choice2 = input('\nYou jump out and your parachute snags in a tree. You detach from your parachute and come to a river crossing.\n'
+                    'Do you cross the river or keep walking in the jungle and try go around? Type cross or go around\n\n')
 
-    if river == "go around":
-        go_around = input("""\nAs you are walking you find an old abandoned town in the jungle. \nThere is a restaurant and near the restaurant there is a small boat. \nThere is also a small airport hangar with a helicopter and there appears to be an old jeep. \nDo you decide to take the helicopter, the boat, the jeep or stay at the restaurant?
-Type helicopter, boat, jeep or restaurant\n\n""")
-        if go_around == "helicopter":
-            print("You start the helicopter and as the helicopter lifts off it explodes. Game Over.")
-        elif go_around == "boat":
+    if choice2 == 'go around':
+        go_around = input('\nAs you are walking you find an old abandoned town in the jungle.\n There is a restaurant and near the restaurant there is a small boat. \nThere is also a small airport hangar with a helicopter and an old jeep. \nDo you take the helicopter, the boat, the jeep or stay at the restaurant?\n '
+                          'Type helicopter, boat, jeep or restaurant\n\n')
+        if go_around == 'helicopter':
             print(
-                "As your boat begins down the river it becomes apparent there is a leak. \nThe boat sinks. Game Over.")
-        elif go_around == "jeep":
-            print("The jeep runs out of gas and you are eaten by jungle animals. Game Over.")
+                'You start the helicopter and as the helicopter lifts off it explodes. Game Over.')
+        elif go_around == 'boat':
+            print(
+                'As your boat begins down the river it becomes apparent there is a leak. \nThe boat sinks. Game Over.')
+        elif go_around == 'jeep':
+            print(
+                'The jeep runs out of gas and you are eaten by jungle animals. Game Over.')
         else:
-            print("You Win!")
+            print('You Win!')
     else:
         print("As you start crossing the river a crocodile eats you. Game Over.")
 else:

72375c15ec54ccab61cfe2f2809c8e7ae32c02d0
Eli Hansen <15258789+emh68@users.noreply.github.com>
2024-05-23 10:18:31 -0600

    Initial commit

--- Average Height/average_height.py
+++ Average Height/average_height.py
@@ -1,13 +1,19 @@
+# Code to practice using for loops
 student_heights = input("Input a list of student heights ").split()
+int_student_heights = []
 for n in range(0, len(student_heights)):
-    student_heights[n] = int(student_heights[n])
+    int_student_heights.append(int(student_heights[n]))
+
 
 total_height = 0
-for height in student_heights:
+for height in int_student_heights:
     total_height += height
 
 number_students = 0
 for students in student_heights:
     number_students += 1
 
-print(round(total_height/number_students))
+avg_height = (round(total_height/number_students))
+
+print(
+    f"total height = {total_height}\nnumber of students = {number_students}\naverage height = {avg_height}")

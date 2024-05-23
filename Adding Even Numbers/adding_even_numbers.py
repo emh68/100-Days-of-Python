8ebb6b0727749df650d99216e066b0cd0b033154
Eli Hansen <15258789+emh68@users.noreply.github.com>
2024-05-23 10:56:06 -0600

    Initial commit

--- Adding Even Numbers/adding_even_numbers.py
+++ Adding Even Numbers/adding_even_numbers.py
@@ -1,7 +1,7 @@
 # This code adds all of the even numbers from 0 to the number the user inputs
 target = int(input("Enter a number between 0 and 1000: "))
 
-total = 0
-for number in range(0, target + 1, 2):
-    total += number
-print(total)
+even_sum = 0
+for number in range(2, target + 1, 2):
+    even_sum += number
+print(even_sum)
